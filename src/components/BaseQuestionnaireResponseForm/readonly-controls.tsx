8e12e15f45d6ab9789cf12fd6903c6df1b3ae079
Alex Lipovka <alex.lipovka@gmail.com>
2026-05-04 18:47:15 +0400

    reuse default controls from web-item-controls

--- src/components/BaseQuestionnaireResponseForm/readonly-controls.tsx
+++ src/components/BaseQuestionnaireResponseForm/readonly-controls.tsx
@@ -7,65 +7,23 @@ import {
 
 import { Barcode } from '@beda.software/web-item-controls/controls';
 import {
-    AnxietyScore,
-    AudioAttachment,
-    Col,
-    DepressionScore,
-    Display,
-    Group,
-    GroupWizardVertical,
-    MarkdownCard,
-    MarkdownDisplay,
-    MarkdownRenderControl,
-    NavigationGroup,
-    QuestionBoolean,
-    QuestionChoice,
-    QuestionDateTime,
-    QuestionDecimal,
-    QuestionInteger,
-    QuestionQuantity,
-    QuestionReference,
-    QuestionText,
-    Row,
-    TextWithInput,
-    TimeRangePickerControl,
-    UploadFile,
+    itemComponents as defaultItemComponents,
+    groupComponent as defaultGroupComponent,
+    itemControlComponents as defaultItemControlComponents,
+    groupControlComponents as defaultGroupControlComponents,
 } from '@beda.software/web-item-controls/readonly-controls';
 
 export const itemComponents: QuestionItemComponentMapping = {
-    text: QuestionText,
-    time: QuestionDateTime,
-    string: QuestionText,
-    integer: QuestionInteger,
-    decimal: QuestionDecimal,
-    quantity: QuestionQuantity,
-    choice: QuestionChoice,
-    date: QuestionDateTime,
-    dateTime: QuestionDateTime,
-    reference: QuestionReference,
-    display: Display,
-    boolean: QuestionBoolean,
-    attachment: UploadFile,
+    ...defaultItemComponents,
 };
 
-export const groupComponent: GroupItemComponent = Group;
+export const groupComponent: GroupItemComponent = defaultGroupComponent;
 
 export const itemControlComponents: ItemControlQuestionItemComponentMapping = {
-    'inline-choice': QuestionChoice,
-    'anxiety-score': AnxietyScore,
-    'depression-score': DepressionScore,
-    'input-inside-text': TextWithInput,
-    'audio-recorder-uploader': AudioAttachment,
+    ...defaultItemControlComponents,
     barcode: Barcode,
-    markdown: MarkdownDisplay,
-    'markdown-card': MarkdownCard,
-    'markdown-editor': MarkdownRenderControl,
 };
 
 export const groupControlComponents: ItemControlGroupItemComponentMapping = {
-    col: Col,
-    row: Row,
-    'time-range-picker': TimeRangePickerControl,
-    'wizard-navigation-group': NavigationGroup,
-    'wizard-vertical': GroupWizardVertical,
+    ...defaultGroupControlComponents,
 };
